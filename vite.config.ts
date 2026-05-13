3fcaaa0d0ec96c359d90a0ee385e8848f4728347
abdharis12 <abdharis.datuamas@gmail.com>
2026-05-13 08:27:10 +0700

    Fix : Vite

--- vite.config.ts
+++ vite.config.ts
@@ -26,4 +26,9 @@ export default defineConfig({
     esbuild: {
         jsx: 'automatic',
     },
+
+    optimizeDeps: {
+        // Tambahkan ini untuk mencegah Vite mengutak-atik library analytics
+        exclude: ['@vercel/analytics']
+    },
 });
